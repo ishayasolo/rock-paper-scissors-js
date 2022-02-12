ce437a0fc4bd62f04f226f9c7d79b57c0f98ae04
ishayasolo <ishaya808@gmail.com>
2022-02-12 01:19:40 +0100

    replace user input from prompt to button click

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ function computerPlay() {
   return options[Math.floor(Math.random() * 3)]
 }
 
-function play(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return `it's a tie!`;
   } else if (playerSelection === 'rock') {
@@ -39,19 +37,52 @@ function play(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('rock, paper or scissors?');
-    let computerSelection = computerPlay();
-    console.log(`player\'s choice: ${playerSelection.toLowerCase()}\nCPU\'s choice: ${computerSelection}\n${play(playerSelection, computerSelection)}`);
-  }
-  console.log('player\'s score:', playerScore);
-  console.log('CPU\'s score:', cpuScore);
+  const mainContainer = document.getElementById('main-container');
+  const resultContainer = document.createElement('div');
+  const paragraph1 = document.createElement('p');
+  const paragraph2 = document.createElement('p');
+  const paragraph3 = document.createElement('p');
+  const paragraph4 = document.createElement('p');
+  const paragraph5 = document.createElement('p');
+  const paragraph6 = document.createElement('p');
+  const lineBreak = document.createElement('br');
 
-  if (playerScore > cpuScore) {
-    console.log('congratulations!...you have won the game');
-  } else {
-    console.log('you lost the game!');
-  }
+  const buttons = mainContainer.querySelectorAll('button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (playerScore < 5 && cpuScore < 5) {
+        const computerSelection = computerPlay();
+        const playerSelection = button.value;
+        
+        paragraph1.textContent = `player's choice: ${playerSelection}`;
+        resultContainer.appendChild(paragraph1);
+
+        paragraph2.textContent = `CPU's choice: ${computerSelection}`;
+        resultContainer.appendChild(paragraph2);
+        
+        paragraph3.textContent = `${playRound(playerSelection, computerSelection)}`;
+        resultContainer.appendChild(paragraph3);
+        
+        resultContainer.appendChild(lineBreak);
+        
+        paragraph4.textContent = `your score: ${playerScore}`;
+        resultContainer.appendChild(paragraph4);
+
+        paragraph5.textContent = `CPU's score: ${cpuScore}`;
+        resultContainer.appendChild(paragraph5);
+
+        if (playerScore == 5) {
+          paragraph6.textContent = 'congratulations!, you have won the game';
+          resultContainer.appendChild(paragraph6);
+        } else if (cpuScore == 5) {
+          paragraph6.textContent = 'better luck next time...you lost the game to CPU';
+          resultContainer.appendChild(paragraph6);
+        }
+      }
+    });
+  });
+  
+  mainContainer.appendChild(resultContainer);
 }
 
 game();
